e748010fded5e4e55edc414aaa5eaa92271c53d5
Justin Falejczyk <jpf9nn@umsystem.edu>
2025-12-04 14:07:14 -0600

    Demo Content

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from pathlib import Path
+import sys
+
+root = Path(__file__).resolve().parents[1]
+sys.path.insert(0, str(root / "src")) 
+from app import add
+
+def test_add():
+  assert add(5,6) == 11
+
+def test_add():
+  assert add(5, 6) != 10
